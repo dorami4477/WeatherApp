2251484d1fc73bba4cc5ed4d5b0d889dcae4538f
dahyun <dahyun4477@gmail.com>
2024-07-13 17:15:56 +0900

    Feat : Configure CoreLocation

--- WeatherApp/Controllers/MapViewController.swift
+++ WeatherApp/Controllers/MapViewController.swift
@@ -7,17 +7,19 @@
 
 import UIKit
 import MapKit
+import CoreLocation
 
-
-class MapViewController: BaseViewController {
+final class MapViewController: BaseViewController {
 
     private var mapView: MKMapView!
     
- 
+    private let locationManager = CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        configureMap(lat: 37.27543611, lon: 127.4432194, title: "test", weather: "Test")
+        locationManager.delegate = self
     }
+    
     override func configureHierarchy() {
         mapView = MKMapView(frame: view.bounds)
         view.addSubview(mapView)
@@ -32,16 +34,77 @@ class MapViewController: BaseViewController {
     override func configureUI() {
         navigationController?.isToolbarHidden = true
     }
-    func configureMap(lat:Double, lon:Double, title:String, weather:String) {
+    
+    private func configureMap(lat:Double, lon:Double, title:String, subtitle:String?) {
         let center = CLLocationCoordinate2D(latitude: lat, longitude: lon)
         let region = MKCoordinateRegion(center: center, span: MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05))
-        mapView.setRegion(region, animated: false)
+        mapView.setRegion(region, animated: true)
         
         let annotation = MKPointAnnotation()
         annotation.coordinate = CLLocationCoordinate2D(latitude: lat, longitude: lon)
         annotation.title = title
-        annotation.subtitle = weather
+        annotation.subtitle = subtitle
         mapView.addAnnotation(annotation)
     }
 
+}
+
+
+extension MapViewController:CLLocationManagerDelegate{
+    //- 사용자 위치를 성공적으로 가지고 온 경우
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let coordinate = locations.last?.coordinate{
+            configureMap(lat: coordinate.latitude, lon: coordinate.longitude, title: "내위치", subtitle: nil)
+        }
+        locationManager.stopUpdatingLocation()
+    }
+
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: any Error) {
+        print(#function)
+    }
+    
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        print(#function, "iOS14+")
+        checkDeviceLocationAuthorization()
+    }
+
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        print(#function, "iOS14-")
+    }
+    
+    func checkDeviceLocationAuthorization(){
+        DispatchQueue.global().async {
+            if CLLocationManager.locationServicesEnabled(){
+                self.checkCurrentLocationAuthorization()
+            }else{
+                print("해당 아이폰의 위치 서비스가 꺼져있습니다.")
+            }
+        }
+
+    }
+    
+    func checkCurrentLocationAuthorization(){
+        var status:CLAuthorizationStatus
+        
+        if #available(iOS 14.0, *){
+            status = locationManager.authorizationStatus
+        }else{
+            status = CLLocationManager.authorizationStatus()
+        }
+        
+        switch status {
+        case .notDetermined:
+            print(status)
+            locationManager.desiredAccuracy = kCLLocationAccuracyBest
+            locationManager.requestWhenInUseAuthorization() //권한 설정 메시지 띄우기
+        case .denied:
+            print(status)
+            print("iOS 설정 창으로 이동하라는 얼럿을 띄워주기")
+        case .authorizedWhenInUse:
+            print(status)
+            locationManager.startUpdatingLocation()
+        default:
+            print(status)
+        }
+    }
 }
